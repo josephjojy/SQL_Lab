4654d526699218c6a8eb9609024d33b97a2a7305
Thejus Paul <32426070+Thejus-Paul@users.noreply.github.com>
2019-08-31 12:01:27 +0530

    Add files via upload

--- Exp 4/Stipend_Amount.sql
+++ Exp 4/Stipend_Amount.sql
@@ -4,11 +4,11 @@ DECLARE
 	stipend student.stipend%type;
 	amount number:=&num;
 	CURSOR display IS
-	SELECT id, name FROM student WHERE stipend<amount;
+	SELECT id, name, stipend FROM student WHERE stipend<amount;
 BEGIN
 	OPEN display;
 	LOOP
-		FETCH display INTO id, name,stipend;
+		FETCH display INTO id,name,stipend;
 		EXIT WHEN display%NOTFOUND;
 			dbms_output.put_line(id || ' ' || name || ' ' || stipend);
 	END LOOP;
