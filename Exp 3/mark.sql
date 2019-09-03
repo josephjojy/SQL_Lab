65e7250194a3db8d568d4f00f4cc251e4fc67576
Thejus Paul <32426070+Thejus-Paul@users.noreply.github.com>
2019-09-03 12:00:45 +0530

    Update mark.sql

--- Exp 3/mark.sql
+++ Exp 3/mark.sql
@@ -1,9 +1,9 @@
-CREATE TABLE nstudent(rollno NUMBER,name VARCHAR(30),m1 NUMBER,m2 NUMBER,m3 NUMBER,tot_marks NUMBER);
-CREATE TABLE grada(rollno NUMBER, name VARCHAR(30),tot_marks NUMBER);
-CREATE TABLE gradb(rollno NUMBER, name VARCHAR(30),tot_marks NUMBER);
-CREATE TABLE gradc(rollno NUMBER, name VARCHAR(30),tot_marks NUMBER);
+CREATE TABLE student(rollno NUMBER,name VARCHAR(30),m1 NUMBER,m2 NUMBER,m3 NUMBER,tot_marks NUMBER);
+CREATE TABLE gradeA(rollno NUMBER, name VARCHAR(30),tot_marks NUMBER);
+CREATE TABLE gradeB(rollno NUMBER, name VARCHAR(30),tot_marks NUMBER);
+CREATE TABLE gradeC(rollno NUMBER, name VARCHAR(30),tot_marks NUMBER);
 
-DELARE
+DECLARE
 	rollno NUMBER:=&rollno;
 	name VARCHAR(30):='&name';
 	m1 NUMBER:=&m1;
@@ -14,11 +14,10 @@ BEGIN
 	tot_marks:=(m1+m2+m3)/3;
 	INSERT INTO nstudent VALUES(rollno,name,m1,m2,m3,tot_marks);
 	IF tot_marks>75 THEN
-		INSERT INTO grada VALUES(rollno,name,tot_marks);
+		INSERT INTO gradeA VALUES(rollno,name,tot_marks);
 	ELSIF tot_marks>60 THEN
-		INSERT INTO gradb VALUES(rollno,name,tot_marks);
+		INSERT INTO gradeB VALUES(rollno,name,tot_marks);
 	ELSE
-		INSERT INTO gradc VALUES(rollno,name,tot_marks);
+		INSERT INTO gradeC VALUES(rollno,name,tot_marks);
 	END IF;
 END;
-
